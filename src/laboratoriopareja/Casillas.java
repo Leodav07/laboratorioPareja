379d05c1b6041290a518d1724637948614832502
Leonardo López <lopezvelasquezleonardodavid@gmail.com>
2025-06-06 13:39:49 -0600

    memorama

--- src/laboratoriopareja/Casillas.java
+++ src/laboratoriopareja/Casillas.java
@@ -3,11 +3,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package laboratoriopareja;
-
+import javax.swing.*;
+import java.awt.*;
+import static java.awt.Image.*;
 /**
  *
  * @author hnleo
  */
 public class Casillas {
+    private String ruta;
+    private ImageIcon imagen;
+    private boolean voltear, encontrado;
     
+    public Casillas(String ruta){
+        this.ruta = ruta;
+        voltear = false;
+        encontrado = false;
+    }
+    
+    private void imagenCasilla(){
+        ImageIcon casillaImagen = new ImageIcon(ruta);
+        imagen = new ImageIcon(casillaImagen.getImage().getScaledInstance(100, 100, SCALE_SMOOTH));
+    }
+    
+    public String getRuta(){
+        return ruta;
+    }
+    public ImageIcon getImagen(){
+        return imagen;
+    }
+    public boolean volteado(){
+        return voltear;
+    }
+    public boolean encontrado(){
+        return encontrado
+    }
+            
 }
